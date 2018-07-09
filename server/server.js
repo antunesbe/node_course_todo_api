3c792b54a9b03ea429451183cf76c3d4599347ca
Breno Antunes <brenoa@br.ibm.com@MacBook-Pro-de-Breno.local>
2018-07-09 13:24:03 -0300

    Add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo')
@@ -25,6 +26,21 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find({}).then(todos => {
         res.status(200).send({todos});
+    }).catch(err => {
+        res.status(500).send(err);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('');
+    }
+    Todo.findById(id).then(todo => {
+        if(!todo){
+            return res.status(404).send('Not found');
+        }
+        res.status(200).send(todo);
     }).catch(err => {
         res.status(500).send(err);
     })
